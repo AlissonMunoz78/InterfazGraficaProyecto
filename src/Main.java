421e6e969ca56f9b466149ba61e93952018a4f84
Alisson Muñoz <alisson.munoz@epn.edu.ec>
2024-06-20 09:55:53 -0500

    6 cambio

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,11 @@ public class Main {
         int numero1 = Integer.parseInt(primerNumero);
         int numero2 = Integer.parseInt(segundoNumero);
         int suma = numero1 + numero2;
+        int resta = numero1 - numero2;
+        int multiplicacion = numero1 * numero2;
+        int division = numero1 / numero2;
 
-        JOptionPane.showMessageDialog(null, "El resultado es: " + suma);
+        JOptionPane.showMessageDialog(null, "El resultado es: " + suma, "suma de dos enteros", JOptionPane.INFORMATION_MESSAGE);
+        JOptionPane.showInputDialog("El resultado es: "+resta);
+
 }}
